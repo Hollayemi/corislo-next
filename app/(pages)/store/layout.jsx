129beb1312deb4a7bd91a330b03b0c4e9b0196e2
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-07-22 01:01:36 +0100

    after netlify

--- app/(pages)/store/layout.jsx
+++ app/(pages)/store/layout.jsx
@@ -1,10 +1,10 @@
 'use client';
 import StoreDashboardLayout from "@/app/components/layouts/StoreDashboardLayout";
 
-export const metadata = {
-  title: "Corislo",
-  description: "Showcase your store now",
-};
+// export const metadata = {
+//   title: "Corislo",
+//   description: "Showcase your store now",
+// };
 
 export default function MyStoreDashboardLayout({ children }) {
     
